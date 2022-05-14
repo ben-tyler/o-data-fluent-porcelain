60ac61d447186105b8610c5447e21ec4de3bfe92
btyler_biscit <btyler@biscit.com.au>
2022-05-14 11:50:09 +1000

    initial idea

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # o-data-fluent-porcelain
+
+use tsx index.ts to compile
+
+use node index.js to run
--- index.ts
+++ index.ts
@@ -1,50 +1,104 @@
+type NTypes = string | 'deferred'
+
 class N {
-  parent: N
-  value: string
-  left: N
-  right: N
-}
+  data: NTypes;
+  left: N;
+  right: N;
+  parent: N;
 
-class ShouldSetField extends N {
-  setField(field: string){
-    this.left.value = field;
-    return this.left as ShouldSetOperator;
+  constructor(data: NTypes, parent?: N) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+    this.parent = parent? parent : null;
   }
 }
 
-class ShouldSetOperator extends N {
-  isEqualTo(){
-    this.parent.value = '=';
-    return this.parent.right as ShouldSetValue;
+const inOrder = (n: N) => {
+  if(n === null) {
+    return;
   }
+  inOrder(n.left);
 
-}
+  console.log(n.data);
 
-class ShouldSetValue extends N {
-  setValue(value: string) {
-    this.value = value;
-    return this.parent.parent as N;
-  }
-}
-
-class Builder extends ShouldSetField {
-  constructor(){
-    super();
-    this.left = new ShouldSetOperator();
-    this.left.parent = this;
-    this.right = new ShouldSetValue();
-    this.right.parent = this;
-  }
+  inOrder(n.right)
 }
 
 
-let root: N = new N();
 
-const foo = new Builder()
-  .setField("Foo").isEqualTo();//.setValue("Bar");
+let queue: Fluent[] = [];
+let root: N = null;
+let pointer: N = null;
 
-console.log(foo);
+class Fluent {}
+class WhereJar extends Fluent {
+  field = (name: string) => {
+    root = new N('deferred');
+    pointer = root;
+    return new Where().field(name);
+  };
+}
 
-// ( R (EqualTo
-//       Warehouse ASM )
+class Where extends Fluent {
+  field = (name: string) => {
+    let exp = new N('deferred', pointer);
+    pointer.left = exp;
+    let field = new N(name, exp);
+    exp.left = field;
+    pointer = exp;
+    return new Condition()
+  }
+}
 
+class Condition extends Fluent {
+  equals = () => {
+    pointer.data = "=";
+    return new Value();
+  }
+}
+
+class Value extends Fluent {
+  value = (value: string) => {
+    let nValue = new N(value, pointer);
+    pointer.right = nValue;
+    return new NextExpression();
+  }
+}
+
+class NextExpression extends Fluent {
+  and = () => {
+    let exp = pointer.parent;
+    exp.data = "AND";
+    let nextExp = new N('deferred', exp);
+    exp.right = nextExp;
+    pointer = nextExp;
+    return new Where();
+  }
+  
+  or = () => {
+    let exp = pointer.parent;
+    exp.data = "OR";
+    let nextExp = new N('deferred', exp);
+    exp.right = nextExp;
+    pointer = nextExp;
+    return new Where();
+  }
+}
+
+
+
+
+new WhereJar().field("Foo").equals().value("Bar")
+  .and().field("foo2").equals().value("Bar2")
+  .or().field("POP").equals().value("OPP");
+
+console.log(root);
+inOrder(root);
+/*
+const expFoo = new N("=");
+expFoo.left = new N("Foo");
+expFoo.right = new N("Bar");
+
+inOrder(expFoo)
+*/
